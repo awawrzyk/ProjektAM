bc68bb6660dc87043a45360fe58d388e1905a14d
AdamTheProgrammer <48458422+awawrzyk@users.noreply.github.com>
2024-01-09 20:25:23 +0100

    Dodanie obsługi pobierania danych przez API do widoku listy klientów

--- src/Screens/CustomersScreen.js
+++ src/Screens/CustomersScreen.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text,Button, SafeAreaView, StyleSheet, View, FlatList} from 'react-native';
+import axios from 'axios';
 
 
 
 export default CustomersScreen = () =>{
+
+    const [customers, setCustomers] = useState([]) //Ustawienie stanów początkowych dla bazy klientów
+
+    //Funkcja która pobiera dane z serwera api ip_address:port/klienci
+    //Testy zostały przeprowadzone na adresie 127.0.0.1 (localhost) oraz porcie 3000
+    useEffect(()=>{
+        //Zapytanie do serwera API za pomocą biblioteki axios
+        axios.get('http://192.168.0.37:3000/klienci')
+        .then(response => setCustomers(response.data)) //Zapisanie danych do stanu
+        .catch(error => console.log('Błąd pobierania danych', error)) //Obsługa wyjątku w przypadku błędu przy pobieraniu danych
+    },[])
+
     return(
         <SafeAreaView style={styles.container}>
             <Text style={styles.textHeader}>{'Klienci'}</Text>
@@ -11,8 +24,8 @@ export default CustomersScreen = () =>{
                 <Button title='Dodaj' color='#23A403' />
                 <View style={styles.buttonSpacing} />
                 <FlatList 
-                    data={[{key: 'Klient 1'},{key: 'Klient 2'},{key: 'Klient 3'}]} //Teraz są wrzucone dane testowe
-                    renderItem={({item}) => <Text style={styles.listItem}>{item.key}</Text>}
+                    data={customers} //Teraz są wrzucone dane testowe
+                    renderItem={({item}) => <Text style={styles.listItem}>{item.imie} {item.nazwisko}</Text>}
                 />
             </View>
         </SafeAreaView>
