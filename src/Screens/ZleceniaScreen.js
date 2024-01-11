484cf55a91e928e619924bdcf4d8ae8dceaa89e6
Skotnicki Kacper <kacper.skotnicki002@gmail.com>
2024-01-11 21:18:35 +0100

    Dodanie pobierania danych przez API do ekranu Zleceń

--- src/Screens/ZleceniaScreen.js
+++ src/Screens/ZleceniaScreen.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text,Button, SafeAreaView, StyleSheet, View, FlatList} from 'react-native';
+import axios from 'axios';
 
 
 
 export default ZleceniaScreen = () =>{
+
+    const [Zlecenia, setZlecenia] =useState([]) //Ustawienie stanów początkowych dla bazy zlecen
+
+    //Funkcja która pobiera dane z serwera api ip_address:port/zlecenia_serwisowe
+    //Testy zostały przeprowadzone na adresie 127.0.0.1 (localhost) oraz porcie 3000
+    useEffect(()=>{
+        //Zapytanie do serwera API za pomocą biblioteki axios
+        axios.get('http://192.168.1.8:3000/zlecenia_serwisowe')
+        .then(response => setZlecenia(response.data)) //Zapisanie danych do stanu
+        .catch(error => console.log('Błąd pobierania danych', error)) //Obsługa wyjątku w przypadku błędu przy pobieraniu danych
+    },[])
+
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.textHeader}>{'Zlecenia'}</Text>
+            <Text style={styles.textHeader}>{'Zlecenia Serwisowe'}</Text>
             <View style={styles.buttonContainer}>
                 <Button title='Dodaj' color='#23A403' />
                 <View style={styles.buttonSpacing} />
                 <FlatList
-                    data={[{key: 'Zlecenie 1'},{key: 'Zlecenie 2'},{key: 'Zlecenie 3'}]} //Teraz są wrzucone dane testowe
-                    renderItem={({item}) => <Text style={styles.listItem}>{item.key}</Text>}
+                    data={Zlecenia}
+                    renderItem={({item}) => <Text style={styles.listItem}>{item.id} {item.opis} {item.status} {item.klient_id}</Text>}
                 />
             </View>
         </SafeAreaView>
@@ -31,7 +44,7 @@ const styles = StyleSheet.create({
     },
     listItem:{
         padding: 10,
-        fontSize: 20,
+        fontSize: 16,
         height: 50,
         backgroundColor: '#66B2FF',
         borderWidth: 5,
