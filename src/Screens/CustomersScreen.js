93ea5c80199f1f6766781abd3d80a1b546340f41
Skotnicki Kacper <kacper.skotnicki002@gmail.com>
2024-01-12 14:27:49 +0100

    poprawa błędu przy dodawaniu klienta w customer screen

--- src/Screens/CustomersScreen.js
+++ src/Screens/CustomersScreen.js
@@ -15,19 +15,19 @@ export default CustomersScreen = () =>{
     //Testy zostały przeprowadzone na adresie 127.0.0.1 (localhost) oraz porcie 3000
     useEffect(()=>{
         //Zapytanie do serwera API za pomocą biblioteki axios
-        axios.get('http://192.168.1.8:3000/klienci')
+        axios.get('http://192.168.0.164:3000/klienci')
         .then(response => setCustomers(response.data)) //Zapisanie danych do stanu
         .catch(error => console.log('Błąd pobierania danych', error)) //Obsługa wyjątku w przypadku błędu przy pobieraniu danych
     },[])
 
     // Funkcja która dodaje nowego kleitna do bazy danych
     const addCustomer = () => {
-        axios.post('http://192.168.1.8:3000/klienci', {
+        axios.post('http://192.168.0.164:3000/klienci', {
             imie: newCustomerName,
             nazwisko: newCustomerLastName
         })
         .then(response => {
-            axios.get('http://192.168.1.8:3000/klienci')
+            axios.get('http://192.168.0.164:3000/klienci')
             .then(response => setCustomers(response.data))
             .catch(error => console.log('Błąd pobierania danych', error))
         })
@@ -50,8 +50,8 @@ export default CustomersScreen = () =>{
                     renderItem={({item}) => <Text style={styles.listItem}>{item.imie} {item.nazwisko}</Text>}
                 />
             </View>
-            {/*Modal dodawania nowego klienta*/}
-            <Modal
+
+            <Modal //Modal dodawania nowego klienta
                 animationType="slide"
                 transparent={true}
                 visible={modalVisible}
@@ -119,4 +119,3 @@ const styles = StyleSheet.create({
         paddingLeft: 10
     }
 });
-
